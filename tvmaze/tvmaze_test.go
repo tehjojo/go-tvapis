7bf5726684feb05cc917db17468d87442918e780
Michael Robinson <michael@michaelrobinson.io>
2018-04-08 14:52:08 -0600

    improved error handling, rewrote tests using std lib subtest instead of goconvey

--- tvmaze/tvmaze_test.go
+++ tvmaze/tvmaze_test.go
@@ -1,105 +1,95 @@
 package tvmaze
 
 import (
-	"encoding/json"
 	"testing"
 
-	log "github.com/Sirupsen/logrus"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTVMaze(t *testing.T) {
-	log.SetLevel(log.DebugLevel)
-	Convey("I have setup a TVMaze client", t, func() {
-		c := DefaultClient
+	c := DefaultClient
 
-		Convey("I can find a show", func() {
-			results, err := c.FindShows("archer")
-			So(err, ShouldBeNil)
-			So(len(results), ShouldBeGreaterThan, 0)
-			//fmt.Println(JSONString(results))
-			So(results[0].Show.GetTitle(), ShouldNotBeBlank)
-			So(results[0].Show.GetDescription(), ShouldNotBeBlank)
-		})
-
-		Convey("I can get a show by its tvmaze id", func() {
-			result, err := c.GetShowWithID("315") // Archer
-			So(err, ShouldBeNil)
-			So(result, ShouldNotBeNil)
-			//fmt.Println(JSONString(result))
-			So(result.GetTitle(), ShouldEqual, "Archer")
-			So(result.GetDescription(), ShouldNotBeBlank)
-			So(result.GetTVDBID(), ShouldEqual, 110381)
-			So(result.GetTVRageID(), ShouldEqual, 23354)
-			So(result.GetIMDBID(), ShouldEqual, "tt1486217")
-			So(result.GetMediumPoster(), ShouldNotBeBlank)
-			So(result.GetOriginalPoster(), ShouldNotBeBlank)
-		})
-
-		Convey("I can get a specific episode of a show", func() {
-			result, err := c.GetEpisode(Show{ID: 315}, 4, 5)
-			So(err, ShouldBeNil)
-			So(result, ShouldNotBeNil)
-			So(result.Name, ShouldNotBeBlank)
-			So(result.Summary, ShouldNotBeBlank)
-		})
-
-		Convey("I can get a show by its tvrage id", func() {
-			result, err := c.GetShowWithTVRageID("23354") // Archer
-			So(err, ShouldBeNil)
-			So(result, ShouldNotBeNil)
-			//fmt.Println(JSONString(result))
-			So(result.GetTitle(), ShouldNotBeBlank)
-			So(result.GetDescription(), ShouldNotBeBlank)
-		})
-
-		Convey("I can get a show", func() {
-			result, err := c.GetShow("archer")
-			So(err, ShouldBeNil)
-			So(result, ShouldNotBeNil)
-			//fmt.Println(JSONString(result))
-			So(result.GetTitle(), ShouldNotBeBlank)
-			So(result.GetDescription(), ShouldNotBeBlank)
-		})
-
-		Convey("I can refresh a show", func() {
-			show := Show{ID: 315} // Archer
-			err := c.RefreshShow(&show)
-			So(err, ShouldBeNil)
-			So(show.GetTitle(), ShouldNotBeBlank)
-			So(show.GetDescription(), ShouldNotBeBlank)
-		})
-
-		Convey("I can get episodes for a show", func() {
-			show := Show{ID: 315} // Archer
-			episodes, err := c.GetEpisodes(show)
-			So(err, ShouldBeNil)
-			//fmt.Println(JSONString(episodes))
-			So(len(episodes), ShouldBeGreaterThan, 0)
-		})
-
-		Convey("I can get the next episode for a show", func() {
-			show := Show{ID: 82} // Game of Thrones
-			episode, err := c.GetNextEpisode(show)
-			So(err, ShouldBeNil)
-			So(episode, ShouldNotBeNil)
-			//fmt.Println(JSONString(episode))
-		})
-
-		Convey("null times are parsed correctly", func() {
-			show := Show{ID: 180} // Firefly
-			episodes, err := c.GetEpisodes(show)
-			So(err, ShouldBeNil)
-			//fmt.Println(JSONString(episodes))
-			So(len(episodes), ShouldBeGreaterThan, 0)
-		})
+	t.Run("find show", func(t *testing.T) {
+		t.Parallel()
+		results, err := c.FindShows("archer")
+		require.NoError(t, err)
+		require.NotEmpty(t, results, "expected results")
+		require.NotEmpty(t, results[0].Show.GetTitle())
+		require.NotEmpty(t, results[0].Show.GetDescription())
 	})
-}
 
-func JSONString(val interface{}) string {
-	bytes, err := json.MarshalIndent(val, "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	return string(bytes)
+	t.Run("get show by id", func(t *testing.T) {
+		t.Parallel()
+		result, err := c.GetShowWithID("315") // Archer
+		require.NoError(t, err)
+		require.NotNil(t, result, "expected a result")
+		require.Equal(t, "Archer", result.GetTitle())
+		require.NotEmpty(t, result.GetDescription())
+		require.Equal(t, 110381, result.GetTVDBID())
+		require.Equal(t, 23354, result.GetTVRageID())
+		require.Equal(t, "tt1486217", result.GetIMDBID())
+		require.NotEmpty(t, result.GetMediumPoster())
+		require.NotEmpty(t, result.GetOriginalPoster())
+	})
+
+	t.Run("get episode", func(t *testing.T) {
+		t.Parallel()
+		result, err := c.GetEpisode(Show{ID: 315}, 4, 5)
+		require.NoError(t, err)
+		require.NotNil(t, result, "expected a result")
+		require.NotEmpty(t, result.Name)
+		require.NotEmpty(t, result.Summary)
+	})
+
+	t.Run("get show with tvrage id", func(t *testing.T) {
+		t.Parallel()
+		result, err := c.GetShowWithTVRageID("23354") // Archer
+		require.NoError(t, err)
+		require.NotNil(t, result, "expected a result")
+		require.NotEmpty(t, result.GetTitle())
+		require.NotEmpty(t, result.GetDescription())
+	})
+
+	t.Run("get show by name", func(t *testing.T) {
+		t.Parallel()
+		result, err := c.GetShow("archer")
+		require.NoError(t, err)
+		require.NotNil(t, result, "expected a result")
+		require.NotEmpty(t, result.GetTitle())
+		require.NotEmpty(t, result.GetDescription())
+	})
+
+	t.Run("refresh show", func(t *testing.T) {
+		t.Parallel()
+		show := Show{ID: 315} // Archer
+		err := c.RefreshShow(&show)
+
+		require.NoError(t, err)
+		require.NotEmpty(t, show.GetTitle())
+		require.NotEmpty(t, show.GetDescription())
+	})
+
+	t.Run("get episodes", func(t *testing.T) {
+		t.Parallel()
+		show := Show{ID: 315} // Archer
+		episodes, err := c.GetEpisodes(show)
+		require.NoError(t, err)
+		require.NotEmpty(t, episodes, "expected to get episodes")
+	})
+
+	t.Run("get next episode", func(t *testing.T) {
+		t.Parallel()
+		show := Show{ID: 1864} // Superstore
+		episode, err := c.GetNextEpisode(show)
+		require.NoError(t, err)
+		require.NotNil(t, episode)
+	})
+
+	t.Run("null times", func(t *testing.T) {
+		t.Parallel()
+		show := Show{ID: 180} // Firefly
+		episodes, err := c.GetEpisodes(show)
+		require.NoError(t, err)
+		require.NotEmpty(t, episodes, "expected to get episodes")
+	})
 }
